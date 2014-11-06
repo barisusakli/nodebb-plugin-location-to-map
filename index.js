5772437812f7c34bce08fd0957c12fb181ac5ac7
Barış Soner Uşaklı <barisusakli@gmail.com>
2014-11-06 00:34:44 -0500

    removed markers

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 			return callback(null, data);
 		}
 
-		data.userData.location = '<img class="img-responsive" src="https://maps.googleapis.com/maps/api/staticmap?center=' + data.userData.location + '&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284" />';
+		data.userData.location = '<img class="img-responsive" src="https://maps.googleapis.com/maps/api/staticmap?center=' + data.userData.location + '&zoom=13&size=600x300&maptype=roadmap" />';
 
 		callback(null, data);
 	};
